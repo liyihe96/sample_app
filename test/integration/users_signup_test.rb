6b2455e1edda789fad33659071165831e71a3e79
Yihe Li <liyihe96@gmail.com>
2014-11-05 23:54:01 -0800

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  
+
   test "valid signup information" do
     get signup_path
     name     = "Example User"
@@ -25,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: password }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
